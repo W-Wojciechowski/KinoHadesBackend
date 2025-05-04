96154346d8fef8cfcdd9e58239a31dec51a3b78c
W-Wojciechowski <71882589+W-Wojciechowski@users.noreply.github.com>
2025-05-04 02:03:20 +0200

    test PostConstruct

--- src/main/java/pl/lodz/p/edu/ProjektOro/ProjektOroApplication.java
+++ src/main/java/pl/lodz/p/edu/ProjektOro/ProjektOroApplication.java
@@ -3,11 +3,18 @@ package pl.lodz.p.edu.ProjektOro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ProjektOroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektOroApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logAfterStart() {
+		System.out.println("✅ Aplikacja wystartowała i działa!");
 	}
 
 }
